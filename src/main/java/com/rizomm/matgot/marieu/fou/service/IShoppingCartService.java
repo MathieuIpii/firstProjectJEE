987cad0282a30104a9ab924a54efe195204ce365
MargotElMadi <margotelmadi@MacBook-Pro-de-Margot.local>
2017-01-04 14:49:32 +0100

    modif nom

--- src/main/java/com/rizomm/matgot/marieu/fou/service/IShoppingCartService.java
+++ src/main/java/com/rizomm/matgot/marieu/fou/service/IShoppingCartService.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.util.Map;
 
 /**
- * Created by steven on 17/11/2016.
+ * Created by Mathieu on 17/11/2016.
  */
 @Remote
 public interface IShoppingCartService extends Serializable {
