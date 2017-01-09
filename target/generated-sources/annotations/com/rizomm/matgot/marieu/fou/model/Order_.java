aa3ae84db0e2759f0815e33ee4397e74452a78e4
MargotElMadi <margotelmadi@MacBook-Pro-de-Margot.local>
2017-01-09 15:25:10 +0100

    prix

---
+++ target/generated-sources/annotations/com/rizomm/matgot/marieu/fou/model/Order_.java
@@ -0,0 +1,13 @@
+package com.rizomm.matgot.marieu.fou.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2017-01-09T15:23:54")
+@StaticMetamodel(Order.class)
+public class Order_ { 
+
+    public static volatile SingularAttribute<Order, Long> id;
+
+}
